cbfe06372799b76e22ba2424b30f370aa22cd425
utkarshmishra1 <112176434+utkarshmishra1@users.noreply.github.com>
2024-07-05 03:43:46 +0530

    Quiz app UI

--- Quiz_App/AppDelegate.swift
+++ Quiz_App/AppDelegate.swift
@@ -1,13 +1,14 @@
 //
 //  AppDelegate.swift
-//  Quiz_App
+//  Quizzler-iOS13
 //
-//  Created by utkarsh mishra on 05/07/24.
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
@@ -15,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
     // MARK: UISceneSession Lifecycle
--- Quiz_App/SceneDelegate.swift
+++ Quiz_App/SceneDelegate.swift
@@ -1,8 +1,9 @@
 //
 //  SceneDelegate.swift
-//  Quiz_App
+//  Quizzler-iOS13
 //
-//  Created by utkarsh mishra on 05/07/24.
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
@@ -23,7 +24,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
